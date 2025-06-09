08830a54cccc979b49b803019fa3feaec6f52886
Miguel Lima <up202108659@up.pt>
2025-06-09 17:44:50 +0100

    nome

--- launch.sh
+++ launch.sh
@@ -2,7 +2,7 @@
 #
 #SBATCH --partition=gpu_min8gb     # Reserved partition
 #SBATCH --qos=gpu_min8gb           # QoS level. Must match the partition name. External users must add the suffix "_ext".
-#SBATCH --job-name=testing TCPFormer    # Job name
+#SBATCH --job-name=testingTCPFormer    # Job name
 #SBATCH --output=slurm_%x.%j.out   # File containing STDOUT output
 #SBATCH --error=slurm_%x.%j.err    # File containing STDERR output. If ommited, use STDOUT.
 
