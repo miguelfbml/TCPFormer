d29704fb1b4a2c9759dc9a473335050694e09aa2
Miguel Lima <up202108659@up.pt>
2025-06-09 15:49:51 +0100

    launch

---
+++ launch.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#
+#SBATCH --partition=gpu_min8gb     # Reserved partition
+#SBATCH --qos=gpu_min8gb           # QoS level. Must match the partition name. External users must add the suffix "_ext".
+#SBATCH --job-name=testing_coco    # Job name
+#SBATCH --output=slurm_%x.%j.out   # File containing STDOUT output
+#SBATCH --error=slurm_%x.%j.err    # File containing STDERR output. If ommited, use STDOUT.
+
+echo "Running job in reserved partition"
+
+# Commands / scripts to run (e.g., python3 train.py)
+python3 train_3dhp.py --eval-only --checkpoint checkpoint_mpi --checkpoint-file TCPFormer_mpi_27.pth.tr --config configs/mpi/TCPFormer_mpi_27.yaml
