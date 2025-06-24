8de5d8e67e0cf71471f5ce5bb292e786c56b7713
Miguel Lima <up202108659@up.pt>
2025-06-24 01:07:06 +0100

    novo

--- data/preprocess/data_to_npz_3dhp.py
+++ data/preprocess/data_to_npz_3dhp.py
@@ -28,25 +28,35 @@ def mpii_get_sequence_info(subject_id, sequence):
 
 
 data_path = '/nas-ctm01/datasets/public/mpi_inf_3dhp'
-
-cam_set = [0, 1, 2, 3, 4, 5, 6, 7]
-# joint_set = [8, 6, 15, 16, 17, 10, 11, 12, 24, 25, 26, 19, 20, 21, 5, 4, 7]
+cam_set = [0, 1, 2, 4, 5, 6, 7, 8]
 joint_set = [7, 5, 14, 15, 16, 9, 10, 11, 23, 24, 25, 18, 19, 20, 4, 3, 6]
 
 dic_seq={}
 
-
 for root, dirs, files in os.walk(data_path):
-
     for file in files:
         if file.endswith("mat"):
-
             path = root.split("/")
+            
+            # Debug: Print the path structure
+            print(f"Full path: {root}")
+            print(f"Path components: {path}")
+            print(f"Last folder: '{path[-1]}' (length: {len(path[-1])})")
+            print(f"Second to last folder: '{path[-2]}' (length: {len(path[-2])})")
+            
+            # Check if folders have expected format
+            if len(path[-2]) < 2 or len(path[-1]) < 4:
+                print(f"Skipping due to unexpected folder format")
+                continue
+                
             subject = path[-2][1]
             seq = path[-1][3]
             print("loading %s %s..."%(path[-2],path[-1]))
 
             temp = mpii_get_sequence_info(subject, seq)
+            if temp is None:
+                print(f"Warning: No sequence info found for subject {subject} sequence {seq}")
+                continue
 
             frames = temp[0]
             fps = temp[1]
@@ -60,7 +70,6 @@ for root, dirs, files in os.walk(data_path):
             data_3d = data['univ_annot3'][cam_set]
 
             dic_cam = {}
-            a  = len(data_2d)
             for cam_idx in range(len(data_2d)):
                 data_2d_cam = data_2d[cam_idx][0]
                 data_3d_cam = data_3d[cam_idx][0]
@@ -75,8 +84,6 @@ for root, dirs, files in os.walk(data_path):
 
                 dic_cam.update({str(cam_set[cam_idx]):dic_data})
 
-
             dic_seq.update({path[-2]+" "+path[-1]:[dic_cam, fps]})
-
 
 np.savez_compressed('../motion3d/data_train_3dhp', data=dic_seq)
