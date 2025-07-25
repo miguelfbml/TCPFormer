04176b0b525fe1839084fbf0f2cc7db14f2112b3
Miguel Lima <up202108659@up.pt>
2025-07-25 21:42:55 +0100

    tuning parameters

--- data/preprocess/MediaPipeTest/estimate_3d_pose_realtime.py
+++ data/preprocess/MediaPipeTest/estimate_3d_pose_realtime.py
@@ -49,7 +49,7 @@ class MediaPipe3DPoseEstimator:
             static_image_mode=False,
             model_complexity=2,
             enable_segmentation=False,
-            min_detection_confidence=0.5,
+            min_detection_confidence=0.3,
             min_tracking_confidence=0.3
         )
         
