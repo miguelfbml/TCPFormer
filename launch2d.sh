ab8523280d8203494d7348d1bccc6de1a0cb969f
Miguel Lima <up202108659@up.pt>
2025-08-04 18:12:28 +0100

    new try

--- launch2d.sh
+++ launch2d.sh
@@ -10,4 +10,7 @@ echo "Running job in reserved partition"
 
 # Commands / scripts to run (e.g., python3 train.py)
 python3 data/preprocess/MediaPipeTest/calculate_metrics_mediapipe.py \
-    --config configs/mpi/TCPFormer_mpi_27.yaml --sequence-name TS1 --max-samples 500
+    --config configs/mpi/TCPFormer_mpi_27.yaml \
+    --sequence-name TS1 \
+    --checkpoint checkpoint_mpi \
+    --max-samples 500
