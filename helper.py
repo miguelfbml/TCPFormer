792a215ceb44d38c3d46f41191857dc3ff9e8261
Miguel Lima <up202108659@up.pt>
2025-08-16 19:10:34 +0100

    n

--- helper.py
+++ helper.py
@@ -1,5 +1,5 @@
 import pickle
 
-with open("data/motion3d/H36M-81/test/00000015.pkl.pkl", "rb") as file:
+with open("data/motion3d/H36M-81/test/00000015.pkl", "rb") as file:
     loaded_data = pickle.load(file)
 print(loaded_data)
