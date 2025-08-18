566e3ad5803f3cce9da0c194c3439e0743ff63b8
Miguel Lima <up202108659@up.pt>
2025-08-18 18:18:49 +0100

    nt

---
+++ launchTrain3dhp.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#
+#SBATCH --partition=gpu_min8gb     # Reserved partition
+#SBATCH --qos=gpu_min8gb           # QoS level. Must match the partition name. External users must add the suffix "_ext".
+#SBATCH --job-name=trainTCPFormer3dhp    # Job name
+#SBATCH --output=slurm_%x.%j.out   # File containing STDOUT output
+#SBATCH --error=slurm_%x.%j.err    # File containing STDERR output. If ommited, use STDOUT.
+
+echo "Starting training job for TCPFormer on MPI-INF-3DHP"
+
+# Set wandb API key if needed
+# export WANDB_API_KEY=your_api_key_here
+
+# Commands / scripts to run (e.g., python3 train.py)
+python3 train_3dhp.py \
+    --config configs/mpi/TCPFormer_mpi_27.yaml \
+    --new-checkpoint checkpoint_mpi \
+    --use-wandb \
+    --wandb-name "TCPFormer_MPI_3DHP_training"
