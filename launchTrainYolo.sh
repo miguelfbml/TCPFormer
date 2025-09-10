22cd8bbb2a9d50bd0fb908a9bad8cca686aad282
Miguel Lima <up202108659@up.pt>
2025-09-10 21:55:22 +0100

    a

--- launchTrainYolo.sh
+++ launchTrainYolo.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 #SBATCH --partition=gpu_min32gb     # Reserved partition
 #SBATCH --qos=gpu_min32gb
-#SBATCH --job-name=YoloTrain
+#SBATCH --job-name=YoloTrain_Enhanced
 #SBATCH --output=slurm_%x.%j.out
 #SBATCH --error=slurm_%x.%j.err
 
-echo "Starting YOLO training on MPI-INF-3DHP"
+echo "Starting Enhanced YOLO training on MPI-INF-3DHP for superior keypoint accuracy"
 
 cd data/preprocess/Yolov11
 
@@ -15,7 +15,15 @@ python train.py \
     --epochs 100 \
     --batch-size 4 \
     --img-size 1280 \
-    --lr 0.001 \
+    --lr auto \
     --device 0 \
     --workers 8 \
-    --use-wandb
+    --patience 20 \
+    --cache disk \
+    --pose-loss-weight 17.0 \
+    --kobj-loss-weight 2.5 \
+    --box-loss-weight 7.5 \
+    --cls-loss-weight 0.5 \
+    --dfl-loss-weight 1.5 \
+    --use-wandb \
+    --wandb-project YOLO_MPI_3DHP_Enhanced_Keypoints
