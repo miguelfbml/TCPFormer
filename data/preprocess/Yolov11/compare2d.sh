75f512110d1082fb18b90941c1b6863dc710e968
Miguel Lima <up202108659@up.pt>
2025-09-16 02:56:41 +0100

    a

--- data/preprocess/Yolov11/compare2d.sh
+++ data/preprocess/Yolov11/compare2d.sh
@@ -9,6 +9,6 @@
 echo "Running job in reserved partition"
 
 
-export CUDA_LAUNCH_BLOCKING=0
+export CUDA_LAUNCH_BLOCKING=1
 # Commands / scripts to run (e.g., python3 train.py)
 python3 compare_gt_yolo_2d.py --all --model-path runs/pose/mpi_yolo11x_pose_corrected3/weights/best.pt
