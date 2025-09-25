9b35ee7cd57480080ae63f196f2fbc94bec67ddb
Miguel Lima <up202108659@up.pt>
2025-09-25 19:00:59 +0200

    a

--- data/preprocess/Yolov11/dataInspection/launch.sh
+++ data/preprocess/Yolov11/dataInspection/launch.sh
@@ -9,4 +9,4 @@
 echo "Running job in reserved partition"
 
 # Commands / scripts to run (e.g., python3 train.py)
-python generateTrainAnnot.py --S1 --Seq1 --cam 4
+python generateTrainAnnot.py --S1 --Seq1 --cam 0
