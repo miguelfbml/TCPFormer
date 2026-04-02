67bf0272fdcdb5ecebd8ee841b6ee2fc0e671943
Miguel Lima <up202108659@up.pt>
2026-04-02 01:46:14 +0100

    testing

--- eval3dhp_27frames.sh
+++ eval3dhp_27frames.sh
@@ -2,11 +2,8 @@
 #
 #SBATCH --partition=gpu_min8gb     # Reserved partition
 #SBATCH --qos=gpu_min8gb           # QoS level. Must match the partition name. External users must add the suffix "_ext".
-#SBATCH --job-name=testingTCPFormerT27    # Job name
+#SBATCH --job-name=testTCPFormer_27    # Job name
 #SBATCH --output=slurm_%x.%j.out   # File containing STDOUT output
 #SBATCH --error=slurm_%x.%j.err    # File containing STDERR output. If ommited, use STDOUT.
-
 echo "Running job in reserved partition"
-
-# Commands / scripts to run (e.g., python3 train.py)
-python3 train_3dhp.py --eval-only --checkpoint checkpoint_mpi --checkpoint-file TCPFormer_mpi_27.pth.tr --config configs/mpi/TCPFormer_mpi_27.yaml
+python3 train_3dhp.py --eval-only --checkpoint checkpoint_mpi_27 --checkpoint-file best_epoch.pth.tr --config configs/mpi/TCPFormer_mpi_27.yaml
