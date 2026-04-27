f5014d13a367b3ebe7616ddcd68e73d0866fd285
Miguel Lima <up202108659@up.pt>
2026-04-27 15:45:12 +0100

    nt

---
+++ data/preprocess/Yolov11/compare2dv2.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#
+#SBATCH --partition=gpu_min11gb     # Reserved partition
+#SBATCH --qos=gpu_min11gb           # QoS level. Must match the partition name. External users must add the suffix "_ext".
+#SBATCH --job-name=testingTime    # Job name
+#SBATCH --output=slurm_%x.%j.out   # File containing STDOUT output
+#SBATCH --error=slurm_%x.%j.err    # File containing STDERR output. If ommited, use STDOUT.
+
+echo "Running job in reserved partition"
+
+
+BATCH_SIZE=${BATCH_SIZE:-32}
+DEVICE=${DEVICE:-cuda:0}
+
+# Commands / scripts to run (e.g., python3 train.py)
+python3 compare_gt_yolo_2d.py --sequence TS1 --model-path runs/pose/mpi_yolo11x_pose_corrected3/weights/best.pt
+# python3 compare_gt_yolo_2d.py --all --model-path runs/pose/mpi_yolo11x_pose_enhanced_keypoints/weights/best.pt --img-size 640 --batch-size "$BATCH_SIZE" --device "$DEVICE"
