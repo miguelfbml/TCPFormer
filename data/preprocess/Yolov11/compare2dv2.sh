40f4f81ad566e4be6f5820121e7fac3caa00ce08
Miguel Lima <up202108659@up.pt>
2026-04-27 15:54:57 +0100

    nt

--- data/preprocess/Yolov11/compare2dv2.sh
+++ data/preprocess/Yolov11/compare2dv2.sh
@@ -13,5 +13,5 @@ BATCH_SIZE=${BATCH_SIZE:-32}
 DEVICE=${DEVICE:-cuda:0}
 
 # Commands / scripts to run (e.g., python3 train.py)
-python3 compare_gt_yolo_2d.py --sequence TS1 --model-path runs/pose/mpi_yolo11x_pose_corrected3/weights/best.pt
+python3 compare_gt_yolo_2d.py --all --model-path runs/pose/mpi_yolo11x_pose_corrected3/weights/best.pt
 # python3 compare_gt_yolo_2d.py --all --model-path runs/pose/mpi_yolo11x_pose_enhanced_keypoints/weights/best.pt --img-size 640 --batch-size "$BATCH_SIZE" --device "$DEVICE"
