3756bb5037ea50069f303ce0ab3c769c1bf98955
Miguel Lima <up202108659@up.pt>
2026-04-27 23:06:56 +0100

    nt

--- data/preprocess/Yolov11/compare_selected_frames.sh
+++ data/preprocess/Yolov11/compare_selected_frames.sh
@@ -12,7 +12,7 @@ echo "Running selected-frame GT vs YOLO comparison"
 
 python3 compare_gt_yolo_selected_frames.py \
     --sequence "TS1" \
-    --frames "600 1200 1800 2400 3000 3600 4200 4800 5400 6000" \
+    --frames 600 1200 1800 2400 3000 3600 4200 4800 5400 6000 \
     --model-path "runs/pose/mpi_yolo11x_pose_corrected3/weights/best.pt" \
     --output-dir "comparison_selected_frames" \
     --img-size "640" \
@@ -22,7 +22,7 @@ python3 compare_gt_yolo_selected_frames.py \
 
 python3 compare_gt_yolo_selected_frames.py \
     --sequence "TS2" \
-    --frames "600 1200 1800 2400 3000 3600 4200 4800 5400 6000" \
+    --frames 600 1200 1800 2400 3000 3600 4200 4800 5400 6000 \
     --model-path "runs/pose/mpi_yolo11x_pose_corrected3/weights/best.pt" \
     --output-dir "comparison_selected_frames" \
     --img-size "640" \
@@ -33,7 +33,7 @@ python3 compare_gt_yolo_selected_frames.py \
 
 python3 compare_gt_yolo_selected_frames.py \
     --sequence "TS3" \
-    --frames "600 1200 1800 2400 3000 3600 4200 4800 5400 5800" \
+    --frames 600 1200 1800 2400 3000 3600 4200 4800 5400 5800 \
     --model-path "runs/pose/mpi_yolo11x_pose_corrected3/weights/best.pt" \
     --output-dir "comparison_selected_frames" \
     --img-size "640" \
@@ -44,7 +44,7 @@ python3 compare_gt_yolo_selected_frames.py \
 
 python3 compare_gt_yolo_selected_frames.py \
     --sequence "TS4" \
-    --frames "600 1200 1800 2400 3000 3600 4200 4800 5400 6000" \
+    --frames 600 1200 1800 2400 3000 3600 4200 4800 5400 6000 \
     --model-path "runs/pose/mpi_yolo11x_pose_corrected3/weights/best.pt" \
     --output-dir "comparison_selected_frames" \
     --img-size "640" \
@@ -56,7 +56,7 @@ python3 compare_gt_yolo_selected_frames.py \
 
 python3 compare_gt_yolo_selected_frames.py \
     --sequence "TS5" \
-    --frames "30 60 90 120 150 180 210 240 270 300" \
+    --frames 30 60 90 120 150 180 210 240 270 300 \
     --model-path "runs/pose/mpi_yolo11x_pose_corrected3/weights/best.pt" \
     --output-dir "comparison_selected_frames" \
     --img-size "640" \
@@ -68,7 +68,7 @@ python3 compare_gt_yolo_selected_frames.py \
 
 python3 compare_gt_yolo_selected_frames.py \
     --sequence "TS6" \
-    --frames "50 100 150 200 250 300 350 400 450 490" \
+    --frames 50 100 150 200 250 300 350 400 450 490 \
     --model-path "runs/pose/mpi_yolo11x_pose_corrected3/weights/best.pt" \
     --output-dir "comparison_selected_frames" \
     --img-size "640" \
