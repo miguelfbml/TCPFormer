f5e7535c037a78dcb5d2baff8488132819e96279
Miguel Lima <up202108659@up.pt>
2026-05-28 08:56:09 +0100

    nt

--- data/preprocess/Yolov11/compare_selected_frames_withconf.sh
+++ data/preprocess/Yolov11/compare_selected_frames_withconf.sh
@@ -14,20 +14,23 @@ python3 compare_gt_yolo_selected_frames.py \
     --sequence "TS1" \
     --frames 600 1200 1800 2400 3000 3600 4200 4800 5400 6000 \
     --model-path "runs/pose/mpi_yolo11x_pose_corrected3/weights/best.pt" \
-    --output-dir "comparison_selected_frames" \
+    --output-dir "comparison_selected_frames_confidence" \
     --img-size "640" \
     --batch-size "32" \
-    --device "cuda:0"
+    --device "cuda:0" \
+    --show-yolo-confidence
+
 
 
 python3 compare_gt_yolo_selected_frames.py \
     --sequence "TS2" \
     --frames 600 1200 1800 2400 3000 3600 4200 4800 5400 6000 \
     --model-path "runs/pose/mpi_yolo11x_pose_corrected3/weights/best.pt" \
-    --output-dir "comparison_selected_frames" \
+    --output-dir "comparison_selected_frames_confidence" \
     --img-size "640" \
     --batch-size "32" \
-    --device "cuda:0"
+    --device "cuda:0" \
+    --show-yolo-confidence
 
 
 
@@ -35,10 +38,11 @@ python3 compare_gt_yolo_selected_frames.py \
     --sequence "TS3" \
     --frames 600 1200 1800 2400 3000 3600 4200 4800 5400 5800 \
     --model-path "runs/pose/mpi_yolo11x_pose_corrected3/weights/best.pt" \
-    --output-dir "comparison_selected_frames" \
+    --output-dir "comparison_selected_frames_confidence" \
     --img-size "640" \
     --batch-size "32" \
-    --device "cuda:0"
+    --device "cuda:0" \
+    --show-yolo-confidence
 
 
 
@@ -46,10 +50,11 @@ python3 compare_gt_yolo_selected_frames.py \
     --sequence "TS4" \
     --frames 600 1200 1800 2400 3000 3600 4200 4800 5400 6000 \
     --model-path "runs/pose/mpi_yolo11x_pose_corrected3/weights/best.pt" \
-    --output-dir "comparison_selected_frames" \
+    --output-dir "comparison_selected_frames_confidence" \
     --img-size "640" \
     --batch-size "32" \
-    --device "cuda:0"
+    --device "cuda:0" \
+    --show-yolo-confidence
 
 
 
@@ -58,10 +63,11 @@ python3 compare_gt_yolo_selected_frames.py \
     --sequence "TS5" \
     --frames 30 60 90 120 150 180 210 240 270 300 \
     --model-path "runs/pose/mpi_yolo11x_pose_corrected3/weights/best.pt" \
-    --output-dir "comparison_selected_frames" \
+    --output-dir "comparison_selected_frames_confidence" \
     --img-size "640" \
     --batch-size "32" \
-    --device "cuda:0"
+    --device "cuda:0" \
+    --show-yolo-confidence
 
 
 
@@ -70,7 +76,8 @@ python3 compare_gt_yolo_selected_frames.py \
     --sequence "TS6" \
     --frames 50 100 150 200 250 300 350 400 450 490 \
     --model-path "runs/pose/mpi_yolo11x_pose_corrected3/weights/best.pt" \
-    --output-dir "comparison_selected_frames" \
+    --output-dir "comparison_selected_frames_confidence" \
     --img-size "640" \
     --batch-size "32" \
-    --device "cuda:0"
+    --device "cuda:0" \
+    --show-yolo-confidence
